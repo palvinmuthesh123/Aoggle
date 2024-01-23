e548db6ffd5c2e60f43fb772b72170acadff73df
Palvin Muthesh <spalvinmuthesh@gmail.com>
2024-01-23 17:11:16 +0530

    jkk

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true, parameterLimit:50000}));
+
 app.use(cors({
   origin: '*'
 }));
